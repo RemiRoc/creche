0e26a4824c8c8d2e4d6b9de697946477e123c38a
RemiRoc <rereroro034@gmail.com>
2019-05-14 12:14:08 +0200

    jsp ce que je fais je vas mourru halp

--- projetSiteCreche/appCreche/views.py
+++ projetSiteCreche/appCreche/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
+class IndexView(generic.FormView):
+	template_name = 'appCreche/index.html'
+	context_object_name ='Accueil'
+
+	
+	
+
+
